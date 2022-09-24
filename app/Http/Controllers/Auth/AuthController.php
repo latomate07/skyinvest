6f113b47532224c7a64f7356a2df82afec93dc5a
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 12:23:16 +0200

    Add auth controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function login() {
+        if(auth()->check()) {
+            return back();
+        }
+
+        return view("auth.login");
+    }
+
+    public function signin() {
+        if(auth()->check()) {
+            return back();
+        }
+        
+        return view("auth.signin");
+    }
+}
