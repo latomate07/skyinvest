911e8d1a008859c8cec82251ff1750e98ecd7b3c
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 19:09:53 +0200

    Add fillable element

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,6 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'images',
+        'location',
+        'amount',
+        'campaignTime',
+        'minInvest',
+        'profits_percentage',
+        'ytbVideo',
+        'docs',
+        'type_return_on_investissment'
+    ];
 
     public function user()
     {
