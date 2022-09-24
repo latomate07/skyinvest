c4ae36146e0f2bf54d109fe52c4595a6cb27a71a
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 14:44:13 +0200

    Add auth trait

---
+++ app/Traits/AuthTrait.php
@@ -0,0 +1,59 @@
+<?php 
+namespace App\Traits;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Notifications\WelcomeUserNotification;
+
+trait AuthTrait 
+{
+    /**
+     * Store the request user
+     */
+    function register(StoreUserRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($request->password);
+
+        $user = User::create($validated);
+        auth()->login($user);
+
+        // Notifier l'utilisateur
+        // WelcomeUserNotification::dispatch();
+        
+        return redirect('/')->with('success', 'Votre compte a été crée avec succès.');
+    }
+
+    /**
+     * Login the request user
+     */
+    function login(LoginUserRequest $request)
+    {
+        $credentials = $request->validated();
+        // Login user
+        if(Auth()->attempt($credentials)) { 
+            return redirect('/')->with('success', 'Bon retour parmi nous !');
+        }
+
+        return redirect()->back()->withErrors('Oups ! Aucun utilisateur trouvé.');
+    }
+
+    /**
+     * Logout the request user
+     */
+    function logout(Request $request)
+    {
+        Session::flush();
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
+
+
