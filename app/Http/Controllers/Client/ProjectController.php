56cab28ef61bd00bcd41eec0dafdb8a45a7907da
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 18:45:25 +0200

    Create projectController functions

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -4,11 +4,48 @@ namespace App\Http\Controllers\Client;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProjectController extends Controller
 {
+    use SoftDeletes;
     public function showProjectPage()
     {
         return view('client.project.index');
+    }
+
+    /**
+     * Store project
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Show project
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Update project
+     */
+    public function update()
+    {
+    }
+
+    /**
+     * Edit project
+     */
+    public function edit()
+    {
+    }
+
+    /**
+     * SoftDelete project
+     */
+    public function delete()
+    {
     }
 }
