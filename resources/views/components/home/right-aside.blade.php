6e779fda422969445405f23fdcd15d4a1c519748
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 11:45:45 +0200

    add home component

---
+++ resources/views/components/home/right-aside.blade.php
@@ -0,0 +1,27 @@
+<aside class="aside-block">
+    <div class="card">
+        <h2 class="subtitle">Sponsorisé</h2>
+        <img class="pub" src="http://wpkixx.com/html/socimo/images/resources/ad-widget2.gif" width="100%" height="100%">
+    </div> 
+    <div class="card">
+        <!-- How it works --->
+        <h2 class="subtitle">Comment ça marche ?</h2>
+        <ul class="howitworks">
+            <li class="listItem"><span class="step">1</span>Inscrivez-vous gratuitement</li>
+            <li class="listItem"><span class="step">2</span>Trouvez un projet</li>
+            <li class="listItem"><span class="step">3</span>Discutez avec l'entreprise</li>
+            <li class="listItem"><span class="step">4</span>Investissez en toute sécurité</li>
+            <li class="listItem"><span class="step">5</span>Versement du ROI garanti</li>
+        </ul>
+        <a class="knowMore" href="#" style="display: flex; justify-content: center; color: var(--btn-color);">En savoir plus</a>
+    </div>
+    
+    <div class="card calltoaction">
+        <h2 class="subtitle">Restez informé !</h2>
+        <p class="content">Recevez les nouveaux projets publiés directement dans votre boître mail : </p>
+        <form class="newsletter" method="post">
+            <input class="searchInput" placeholder="Entrez votre mail ici">
+            <input type="submit" value="Go" class="searchBtn">
+        <form>
+    </div>
+</aside>
