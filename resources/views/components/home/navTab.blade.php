de8ffa572576143fc5406db7def3787705f50c87
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 12:41:41 +0200

    Create navTab file

---
+++ resources/views/components/home/navTab.blade.php
@@ -0,0 +1,11 @@
+<div class="navigationTab">
+    <div class="tab">
+        <h4><a class="thirdtitle active" href="#">Accueil</a></h4>
+    </div>
+    <div class="tab">
+        <h4><a class="thirdtitle" href="#">Récent</a></h4>
+    </div>
+    <div class="tab">
+        <h4><a class="thirdtitle" href="#">Favoris</a></h4>
+    </div>
+</div>
