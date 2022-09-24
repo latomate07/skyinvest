ceb590e8aef8a87501d7bbcf318fbb17a3c9ef60
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 16:03:26 +0200

    Add showProjectPage function

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class ProjectController extends Controller
 {
-    //
+    public function showProjectPage()
+    {
+        return view('client.project.index');
+    }
 }
