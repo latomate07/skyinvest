a7dc0cc3cbe0fb9427422e65f7dfede9d6967257
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 16:34:32 +0200

    Add relation between user and project

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function projects()
+    {
+        $this->hasMany(ProjectModel::class);
+    }
 }
