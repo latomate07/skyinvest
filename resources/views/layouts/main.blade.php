05a80058b291eab6a8c5748a84a4e9a3726527b3
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 11:46:14 +0200

    Fix css asset url

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SkyInvest</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
-    @include('partials.header')
-    @yield('content')
-    @include('partials.footer')
+    <div class="container">
+        @include('partials.header')
+        @yield('content')
+        @include('partials.footer')
+    </div>
 </body>
 </html>
