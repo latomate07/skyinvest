105a1f8c0b72ad7e40597d6b77d2fd353037eac3
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 15:38:26 +0200

    Add user id to sessionStorage js

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,9 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/1ba83f2b65.js" crossorigin="anonymous"></script> 
     <script src="{{ asset('assets/js/app.js') }}" defer></script>
+    @auth
+    <script>sessionStorage.setItem('user_id', {{ auth()->user()->id }})</script>
+    @endauth
 </head>
 <body>
     <div class="container">
