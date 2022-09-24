8ec2efd690404450aa0e402eed11a96a6b93dd8d
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 12:41:06 +0200

    Separate navtab content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,24 +4,11 @@
 <div class="main">
     <div class="left">
         @include('components.home.left-aside')
-    </div><!-- End Aside Left -->
-    <div class="middle-block">
-        <div class="navigationTab">
-            <div class="tab">
-                <h4><a class="thirdtitle active" href="#">Accueil</a></h4>
-            </div>
-            <div class="tab">
-                <h4><a class="thirdtitle" href="#">Récent</a></h4>
-            </div>
-            <div class="tab">
-                <h4><a class="thirdtitle" href="#">Favoris</a></h4>
-            </div>
-        </div> <!-- End navigation bar -->
-            <!-- Template du projet -->
-            <?php // include_once('template/project-item.php'); ?>
     </div>
-    <!--ENd Middle Content -->
-
+    <div class="middle-block">
+        @include('components.home.navTab')
+        <?php // include_once('template/project-item.php'); ?>
+    </div>
     <div class="right">
         @include('components.home.right-aside')
     </div>
