29795d5d33a49281b33777bcab158ac4d2267196
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 11:46:27 +0200

    Add footer content

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <img class="footer-img" src="{{ asset('assets/images/footer-img.png') }}" style="width:100%">
+    <p class="copyright" style="padding:5px; text-align:center;">© 2022 TahirouTest - Tous droits réservés</p>
+</footer>
