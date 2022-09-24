f3268d78c73a1cedae951c1824e51eb8338077c3
Barry <barrylaouanmagagi@gmail.com>
2022-09-24 14:42:48 +0200

    Edit authController functions name

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Traits\AuthTrait;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
+    use AuthTrait;
+    public function showLoginPage() {
         if(auth()->check()) {
-            return back();
+            return view('client.dashboard.index');
         }
 
         return view('auth.login');
     }
 
-    public function signin() {
+    public function showSigninPage() {
         if(auth()->check()) {
-            return back();
+            return view('client.dashboard.index');
         }
         
         return view('auth.signin');
