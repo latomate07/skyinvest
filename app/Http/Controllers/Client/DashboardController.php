c469ce2bae3f3afc9b6e00a257e2f5057d7fac71
Barry <barrylaouanmagagi@gmail.com>
2022-09-25 22:46:30 +0200

    Add dashboardTrait trait

--- app/Http/Controllers/Client/DashboardController.php
+++ app/Http/Controllers/Client/DashboardController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Client;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Traits\DashboardTrait;
 
 class DashboardController extends Controller
 {
+    use DashboardTrait;
     public function showDashboardPage()
     {
         return view('client.dashboard.index');
