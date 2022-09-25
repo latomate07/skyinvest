b6c03356b59bffa3acd4c5f0c5ad4580e459c0fa
Barry <barrylaouanmagagi@gmail.com>
2022-09-25 22:46:54 +0200

    Add medias() relation with user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Project::class);
     }
+
+    public function medias()
+    {
+        return $this->hasOne(Medias::class);
+    }
 }
