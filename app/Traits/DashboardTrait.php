e2e4d62292660913020b810c9134f9561548c4c2
Barry <barrylaouanmagagi@gmail.com>
2022-09-25 22:47:43 +0200

    Add DashboardTrait()

---
+++ app/Traits/DashboardTrait.php
@@ -0,0 +1,32 @@
+<?php 
+namespace App\Traits;
+
+use App\Models\Medias;
+use Illuminate\Http\Request;
+
+trait DashboardTrait
+{
+    /**
+     * Ajax controller
+     */
+    public function liveChange(Request $request)
+    {
+        // dd($request->all());
+        $media = new Medias();
+
+        if($request->has('user_logo'))
+        {
+            $path = $request->user_logo->store('/', ['disk' => 'user_logo_path']);
+            $media->url = $path;
+            $media->save();
+        }
+        $data = [
+            'user' => auth()->user(),
+            'path_logo' => $path ?? 'Non défini',
+            'success' => 'Votre photo de profil a été mis à jour avec succès'
+        ];
+        return response()->json($data);
+    }
+}
+
+
