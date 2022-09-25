734ecc2636a033f62b866df7a68ee5a44dfef84d
Barry <barrylaouanmagagi@gmail.com>
2022-09-25 10:23:16 +0200

    Add thumbnail model

---
+++ app/Models/Thumbnail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Thumbnail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'url',
+        'project_id'
+    ];
+
+    public function project()
+    {
+        $this->belongsTo(Project::class);
+    }
+}
