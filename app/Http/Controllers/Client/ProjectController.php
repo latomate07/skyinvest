00dab6f4774ba2b922d098a8fead9428620cef1c
Barry <barrylaouanmagagi@gmail.com>
2022-09-25 10:22:34 +0200

    Add store functions

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -7,10 +7,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Project;
+use App\Models\Thumbnail;
+use App\Models\Categories;
 
 class ProjectController extends Controller
 {
     use SoftDeletes;
+    protected $thumbnail;
+    protected $categorie;
+
+    public function __construct(Thumbnail $thumbnail, Categories $categorie)
+    {
+        $this->thumbnail = $thumbnail;
+        $this->categorie = $categorie;
+    }
     public function showProjectPage()
     {
         return view('client.project.index');
@@ -21,8 +32,27 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        $validated = $request->validated();
-        
+        $project = Project::create($request->except(['images', 'categorie']));
+        foreach($request->images as $image)
+        {
+            $path = $image->store('thumbnail');
+            $this->thumbnail->url = $path;
+            $this->thumbnail->project_id = $project->id;
+            $this->thumbnail->save();
+        }
+        // Store categorie
+        $this->categorie->name = $request->categorie;
+        $this->categorie->project_id = $project->id;
+        if(!$project)
+        {
+            return redirect()->back()->withErrors('Veuillez remplir tout les champs !');
+        }
+        $data = [
+            "project_id" => $project->id,
+            "author_id" => auth()->user()->id,
+            "successMessage" => "Votre projet a été crée avec succès !"
+        ];
+        return response()->json($data, 200);
     }
 
     /**
