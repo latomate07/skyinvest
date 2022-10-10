56c1510a22eecd0b67550678de6719b66d8b8c29
Barry <barrylaouanmagagi@gmail.com>
2022-10-10 22:47:06 +0200

    Add mediable morphs columns

--- database/migrations/2022_09_25_190534_create_medias_table.php
+++ database/migrations/2022_09_25_190534_create_medias_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('url');
             $table->foreignId('user_id')->constrained();
+            $table->morphs('mediable');
             $table->timestamps();
         });
     }
