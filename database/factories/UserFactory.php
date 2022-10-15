c2614d79981e22008e95129918b115bad8bfeacf
Barry <barrylaouanmagagi@gmail.com>
2022-10-15 21:28:42 +0200

    Add some columns to User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'pseudo' => fake()->lastName(),
+            'role' => fake()->randomElement(['Investisseur', 'Entreprise']),
+            'country' => fake()->text(30),
+            'isAgreedWithTerms' => fake()->randomElement(['on', 'off']),
+            'wishNewsletter' => fake()->randomElement(['on', 'off']),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
