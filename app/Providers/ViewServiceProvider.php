e8adf7449698f3d4474a960f7c80e6b549219e77
Barry <barrylaouanmagagi@gmail.com>
2022-10-15 20:21:38 +0200

    Return "null" if user is not logged

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -25,10 +25,14 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Send Medias Collection to all application view
         View::composer('*', function($view){
             if(auth()->check())
             {
                 $view->with('medias', Medias::where('user_id', auth()->user()->id)->first());
+            }
+            else {
+                $view->with('medias', null);
             }
         });
     }
