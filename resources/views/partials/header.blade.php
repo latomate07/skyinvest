8fa3e90416ae45a411d548e40608cc1a8c36b6e3
Barry <barrylaouanmagagi@gmail.com>
2022-10-15 23:48:13 +0200

    Add live search functionnality

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -89,9 +89,11 @@
                     <div class="logoDiv">
                          <h1><a href="/">Logo</a></h1>
                     </div>
-                    <form class="searchBar">
+                    <form class="searchBar" method="POST" id="searchBar">
+                        @csrf
                         <input type="search" placeholder="Trouver un projet..." class="searchInput" id="searchInput">
                         <button type="submit" class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" class="crayons-icon c-btn__icon" focusable="false"><path d="m18.031 16.617 4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"></path></svg></button>
+                        <ul class="resultOfLiveSearch"></ul>
                     </form>
                     @guest
                         <!-- Show this when user is not connected -->
@@ -138,3 +140,44 @@
     </header>
     
     <div id="overlay"></div>
+
+<script>
+        // Hide Result List Block
+        $('.resultOfLiveSearch').hide()
+
+        $('#searchBar').on('submit', function(event){
+                event.preventDefault();
+        })
+        $('#searchInput').on('keyup', function(){
+                $('.resultOfLiveSearch').show()
+                $.ajax({
+                        url: "{{ route('ajax.search') }}",
+                        type: "POST",
+                        data: {
+                                '_token': '{{ csrf_token() }}',
+                                'wishResultTerm': $(this).val()
+                        },
+                        success: function(data){
+                                $.each(data, function(index, value){
+                                        if($('#element-'+index).length !== 1){
+                                                $('.resultOfLiveSearch').append('<li class="resultOfLiveSearchItem" id="'+'element-'+index+'">'+ value.name +'</li>')
+                                        } 
+                                        if($('#element-'+index).length == 0){
+                                                $('#element-'+index).hide()
+                                        }
+                                        console.log(value.name);
+                                })
+                        },
+                        error: function(error){
+                                console.log(error);
+                        }
+                })
+        })
+        $('#searchInput').on('click', function(){
+                $('.resultOfLiveSearch').show()
+        })
+        $('#overlay').on('click', function(){
+                // Hide Result List Block
+                $('.resultOfLiveSearch').hide()
+        })
+</script>
