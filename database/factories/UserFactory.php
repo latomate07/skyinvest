592762555f467bba3f04d1be2522a6f07585d165
Barry <barrylaouanmagagi@gmail.com>
2022-10-21 23:16:47 +0200

    Refactoring + Deletes unusual fake columns

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,23 +17,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $role = fake()->randomElement(['Investisseur', 'Entreprise']);
         return [
             'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'pseudo' => fake()->unique()->lastName(),
-            'role' => fake()->randomElement(['Investisseur', 'Entreprise']),
+            'role' => $role,
+            'enterprise_name' => $role == 'Entreprise' ? fake()->unique()->company() : '',
+            'investor_username' => $role == 'Investisseur' ? fake()->unique()->userName() : '',
             'country' => fake()->text(30),
             'isAgreedWithTerms' => fake()->randomElement(['on', 'off']),
             'wishNewsletter' => fake()->randomElement(['on', 'off']),
-            'neph_number' => Str::random(16),
-            'juridique_status' => fake()->randomElement(['EURL', 'SARL', 'SASU', 'SAS', 'SA']),
-            'bilan_enterprise' => fake()->numberBetween(500, 15000),
-            'adresse' => Str::random(15),
-            'phone_number' => fake()->phoneNumber(),
-            'iban' => Str::random(26),
-            'identity_card' => fake()->url(),
-            'source_of_income' => fake()->randomElement(['Salaire', 'Bénéfices']),
-            'user_profession' => fake()->randomElement(['Salarié', 'Entrepreneur']),
+            'enterprise_description' => fake()->realText(150),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
