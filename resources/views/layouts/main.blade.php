b04c0a62fe799cf13e416b545659784730c295c7
Barry <barrylaouanmagagi@gmail.com>
2022-10-22 16:43:50 +0200

    Add "description" tag

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Le premier réseau social dédié à l'investissement. Faites fructifier votre argent rapidement et éfficacement.">
     <title>SkyInvest</title>
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
