57e2809558e66cb34317f4dea9692fe8a9ee280c
Barry <barrylaouanmagagi@gmail.com>
2022-10-25 20:51:44 +0200

    Sucess notification component

---
+++ resources/views/components/global/success.blade.php
@@ -0,0 +1,10 @@
+<div id="successNotifBlock" class="Message Message--green">
+    <div class="Message-icon">
+      <i class="fa fa-check"></i>
+    </div>
+    <div class="Message-body">
+      <p id="notif_message_title"></p>
+      <p id="notif_message_content"></p>
+    </div>
+    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+</div>
