5aee184a0d17a1287eaccea944a7215baecaa738
Barry <barrylaouanmagagi@gmail.com>
2022-11-05 22:53:45 +0100

    Get Investor Projects liked

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProfilController extends Controller
@@ -13,9 +14,17 @@ class ProfilController extends Controller
     public function index(User $id, Request $request)
     {
         $data = User::find($id)->first();
+        $projects_liked = collect();
+
+        foreach($data->project_liked as $project_liked)
+        {
+            $query = Project::where('id', $project_liked->likeable_id)->first();
+            $projects_liked->push($query);
+        }
 
         return view('client.profil.index', [
-            'data' => $data
+            'data' => $data,
+            'projects_liked' => $projects_liked
         ]);
     }
 }
