d156676d12b2ccafa37e6340de15fa4cb7ecf8c7
Barry <barrylaouanmagagi@gmail.com>
2022-11-05 23:15:43 +0100

    Add error notif component

---
+++ resources/views/components/global/error.blade.php
@@ -0,0 +1,10 @@
+<div id="errorNotifBlock" class="Message Message--red">
+    <div class="Message-icon">
+      <i class="fa fa-exclamation"></i>
+    </div>
+    <div class="Message-body">
+      <p id="error_message_title"></p>
+      <p id="error_message_content"></p>
+    </div>
+    <button class="Message-close js-messageClose"><i class="fa fa-times"></i></button>
+</div>
