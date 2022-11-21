3f1dc3b27579f9b7912f0e75e37643ebc1a76979
Barry <barrylaouanmagagi@gmail.com>
2022-11-21 15:25:06 +0100

    Remove unused column

--- database/migrations/2022_09_25_190534_create_medias_table.php
+++ database/migrations/2022_09_25_190534_create_medias_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('medias', function (Blueprint $table) {
             $table->id();
             $table->string('url');
-            $table->foreignId('user_id')->constrained();
             $table->morphs('mediable');
             $table->timestamps();
         });
