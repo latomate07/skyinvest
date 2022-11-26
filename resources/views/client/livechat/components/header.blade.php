7139939ede3ebd01c6b1f974ddc66482fb36105c
Barry <barrylaouanmagagi@gmail.com>
2022-11-26 16:09:43 +0100

    add header template in single file

---
+++ resources/views/client/livechat/components/header.blade.php
@@ -0,0 +1,16 @@
+<div class="logo">
+    <h3>SkyInvest</h3>
+</div>
+<div class="search-bar">
+    <input type="text" placeholder="Rechercher..." />
+</div>
+<div class="user-settings">
+    <div class="dark-light">
+        <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round"
+            stroke-linejoin="round">
+            <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
+    </div>
+    <img class="user-profile" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png" alt=""
+        class="account-profile" alt="">
+</div>
