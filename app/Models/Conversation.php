2b9f2ed60db304eeef99bc60224603090794bfc4
Barry <barrylaouanmagagi@gmail.com>
2022-11-26 16:57:02 +0100

    Fill conversations models

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,69 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'conversations';
+
+    /**
+     * Fillable attributs
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'uid',
+        'from_id',
+        'to_id',
+        'status'
+    ];
+
+    /**
+     * Get conversation messages
+     * 
+     * @return object
+     */
+    public function messages()
+    {
+        return $this->hasMany(ConversationMessage::class, 'conversation_id');
+    }
+
+    /**
+     * Get user that send the message
+     * 
+     * @return object 
+     */
+    public function sender()
+    {
+        return $this->hasOne(User::class, 'from_id');
+    }
+
+    /**
+     * Get user that receive the message
+     * 
+     * @return object
+     */
+    public function receiver()
+    {
+        return $this->hasOne(User::class, 'to_id');
+    }
+
+    /**
+     * Scope to get conversation that have active status
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('status', 'active');
+    }
+
+    /**
+     * scope to get all user conversations
+     */
+    public function scopeGetAllConversations($query)
+    {
+        return $query->where('from_id', auth()->user()->id);
+    }
 }
--- app/Models/ConversationMessage.php
+++ app/Models/ConversationMessage.php
@@ -8,4 +8,63 @@ use Illuminate\Database\Eloquent\Model;
 class ConversationMessage extends Model
 {
     use HasFactory;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'conversations_messages';
+
+    /**
+     * Fillable attributs
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'uid',
+        'conversation_id',
+        'from_id',
+        'to_id',
+        'message',
+        'is_seen'
+    ];
+
+    /**
+     * Get appropriate conversation
+     * 
+     * @return object
+     */
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    /**
+     * Get user that send the message
+     * 
+     * @return object 
+     */
+    public function sender()
+    {
+        return $this->hasOne(User::class, 'from_id');
+    }
+
+    /**
+     * Get user that receive the message
+     * 
+     * @return object
+     */
+    public function receiver()
+    {
+        return $this->hasOne(User::class, 'to_id');
+    }
+
+    /**
+     * Scope to find a message that seen
+     */
+    public function scopeIsSeen($query)
+    {
+        return $query->where('conversation_id', $this->conversation_id)->where('is_seen', 1);
+    }
 }
