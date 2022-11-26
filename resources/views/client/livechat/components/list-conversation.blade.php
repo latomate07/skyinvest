cdc1fc233edbf425f2f2c74c40e9ef19fff36a67
Barry <barrylaouanmagagi@gmail.com>
2022-11-26 23:05:19 +0100

    Add active class when current user is on message

--- resources/views/client/livechat/components/list-conversation.blade.php
+++ resources/views/client/livechat/components/list-conversation.blade.php
@@ -3,20 +3,20 @@
         <div class="chat-area-title">Messages</div>
     </div>
     @forelse ($conversations as $conversation)
-        <div class="msg online">
-            @if(!is_null($conversation->receiver->medias))
+        <a class="msg online {{ (request()->route()->uid == $conversation->uid) ? "active" : ""}}" href="{{ route('conversations.show', $conversation->uid) }}">
+            @if(!is_null($conversation->receiver?->medias))
                 <img class="msg-profile" src="{{ asset('assets/client/logos/' . $conversation->receiver->medias->url) }}" alt="logo">
             @else
                 <img class="msg-profile" src="{{ asset('assets/client/logos/default.png') }}" alt="logo">
             @endif
-            <a class="msg-detail" href="{{ route('conversations.show', $conversation->uid) }}">
-                <div class="msg-username">{{ ($conversation->receiver->role == "Investisseur") ? $conversation->receiver->investor_username : $conversation->receiver->enterprise_name }}</div>
+            <div class="msg-detail">
+                <div class="msg-username">{{ ($conversation->receiver?->role == "Investisseur") ? $conversation->receiver?->investor_username : $conversation->receiver?->enterprise_name }}</div>
                 <div class="msg-content">
                     <span class="msg-message">Ceci est un test blablablba</span>
                     <span class="msg-date">{{ \Carbon\Carbon::parse($conversation->updated_at)->diffForHumans() }}</span>
                 </div>
-            </a>
-        </div>
+            </div>
+        </a>
     @empty
         <p style="text-align:center">Aucune conversation trouvée.</p>
     @endforelse
